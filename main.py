5d7189a34f4a80586523430965437324c733d34e
Harsh Koli <harshkoli201@gmail.com>
2022-02-13 15:13:52 +0530

    Sorted Words

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import nltk #Natural Language Tool Kit (NLTK) is a Python library to make programs that work with natural language.
 from nltk.stem import LancasterStemmer
 stemmer = LancasterStemmer()
@@ -12,14 +13,46 @@ with open("intents.json") as file:
     data = json.load(file)
 
 words = []
-labels = []
-docs = []
+labels = [] 
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+    
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
